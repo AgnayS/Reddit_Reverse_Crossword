4c8daa904a3871e9049d8e6794295f5cd12a8020
rhit-srivasa <srivasa@rose-hulman.edu>
2024-12-15 19:14:37 +0530

    Made codebase devvit playtest comaptible. Changes made to vite.config.ts, and the runner files.

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,23 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dotenv from 'dotenv';
 
-// Load environment variables from the .env file
 dotenv.config();
 
 export default defineConfig({
-  root: './src', // Set the root directory to "src"
+  root: 'src/web',
+  base: './',
   build: {
-    outDir: '../dist', // Output directory for build
-    emptyOutDir: true, // Clear the output directory before each build
+    outDir: '../../webroot',
+    emptyOutDir: true,
     rollupOptions: {
-      input: './src/index.html', // Specify the entry point
+      input: 'src/web/index.html',
     },
   },
+  plugins: [react()],
   resolve: {
-    extensions: ['.js', '.ts', '.jsx', '.tsx'], // Handle TypeScript and JavaScript files
+    extensions: ['.js', '.ts', '.jsx', '.tsx'],
   },
   define: {
-    'process.env': process.env, // Expose environment variables to the client
+    'process.env': process.env,
   },
 });
